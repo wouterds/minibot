d0abbbf62678a57e5afe8726493921e2b993126d
Wouter <wouter.de.schuyter@gmail.com>
2026-05-16 21:10:10 +0200

    refactor: extract PS3 controller logic into controller module

---
+++ src/controller.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace controller
+{
+
+void begin();
+void update();
+
+} // namespace controller
--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,7 @@
-#include "config.h"
+#include "controller.h"
 #include "leds.h"
 #include "status_led.h"
 #include <Arduino.h>
-#include <Ps3Controller.h>
-
-static void onConnect()
-{
-  Serial.println("PS3 connected");
-  status_led::setConnected(true);
-}
-
-static void onDisconnect()
-{
-  Serial.println("PS3 disconnected");
-  status_led::setConnected(false);
-  leds::allOff();
-}
-
-static void onNotify()
-{
-  if (Ps3.event.button_down.triangle) { leds::toggle(leds::TRIANGLE); Serial.println("△"); }
-  if (Ps3.event.button_down.circle)   { leds::toggle(leds::CIRCLE);   Serial.println("○"); }
-  if (Ps3.event.button_down.cross)    { leds::toggle(leds::CROSS);    Serial.println("✕"); }
-  if (Ps3.event.button_down.square)   { leds::toggle(leds::SQUARE);   Serial.println("□"); }
-}
 
 void setup()
 {
@@ -31,35 +9,10 @@ void setup()
 
   status_led::begin();
   leds::begin();
-
-  Ps3.attach(onNotify);
-  Ps3.attachOnConnect(onConnect);
-  Ps3.attachOnDisconnect(onDisconnect);
-  Ps3.begin(config::HOST_MAC);
-
-  Serial.printf("Waiting for PS3 controller on %s ...\n", config::HOST_MAC);
+  controller::begin();
 }
 
 void loop()
 {
-  if (!Ps3.isConnected())
-  {
-    status_led::blinkWhileWaiting();
-    delay(50);
-    return;
-  }
-
-  int8_t y = Ps3.data.analog.stick.ly;
-  if (abs(y) > config::STICK_DEAD_ZONE)
-  {
-    int delta = -y / config::RAMP_SCALE;
-    int newBrightness = constrain((int)leds::brightness() + delta, 0, 255);
-    if (newBrightness != (int)leds::brightness())
-    {
-      leds::setBrightness((uint8_t)newBrightness);
-      Serial.printf("brightness: %u\n", newBrightness);
-    }
-  }
-
-  delay(20);
+  controller::update();
 }
